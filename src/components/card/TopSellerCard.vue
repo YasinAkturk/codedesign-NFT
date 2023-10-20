ced9f852a45e0772401729f81b4b8a1b9bd0cb2c
Yasin Aktürk <yasnaktrk@gmail.com>
2023-10-20 17:29:04 +0300

    top seller card completed

---
+++ src/components/card/TopSellerCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col items-center gap-[16px]">
+        <img class="w-[102px] h-[102px] rounded-[102px] select-none cursor-pointer" :src="image" alt="Top Seller NFT">
+        <span class="font-medium text-white text-[20x] cursor-pointer">{{ title }}</span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    image: { type: String },
+    title: { type: String }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
