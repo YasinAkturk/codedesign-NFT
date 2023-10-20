470ba58925ebc84f621eb2f61d6b8a4aa6fea74e
Yasin Aktürk <yasnaktrk@gmail.com>
2023-10-20 17:29:32 +0300

    topseller completed

---
+++ src/components/TopSeller.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col gap-[40px] w-full px-[50px] my-[100px] items-center">
+        <span class="font-bold text-white text-[32px]">TOP SELLER</span>
+        <div class="flex gap-[40px]">
+            <TopSellerCard v-for="item in list" :image="item.image" :title="item.title" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import TopSellerCard from "./card/TopSellerCard.vue";
+const list = ref([
+    { image: "images/topSeller/seller1.svg", title: "RizKhen" },
+    { image: "images/topSeller/seller2.svg", title: "Jhiehe" },
+    { image: "images/topSeller/seller3.svg", title: "Fukiep" },
+    { image: "images/topSeller/seller4.svg", title: "Hags" },
+    { image: "images/topSeller/seller5.svg", title: "Berhans" },
+    { image: "images/topSeller/seller6.svg", title: "Abdliie" },
+    { image: "images/topSeller/seller7.svg", title: "Wetur Art" },
+    { image: "images/topSeller/seller8.svg", title: "Pokko" },
+])
+</script>
