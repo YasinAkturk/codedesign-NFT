9f639eb555e2bf4f529efbb6aa56ef02b28effae
Yasin Aktürk <yasnaktrk@gmail.com>
2023-10-20 17:30:10 +0300

    Top Seller and Categories components have been added.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 import Navbar from "./components/Navbar.vue";
 import Slider from "./components/Slider.vue";
 import Content from "./components/Content.vue";
+import TopSeller from "./components/TopSeller.vue";
+import Categories from "./components/Categories.vue";
 </script>
 <template>
   <Navbar />
   <Slider />
   <Content />
+  <TopSeller />
+  <Categories />
 </template>
