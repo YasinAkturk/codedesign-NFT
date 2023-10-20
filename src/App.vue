fae13cd97f6406ff5d8037819ed164d24553e40f
Yasin Aktürk <yasnaktrk@gmail.com>
2023-10-20 16:17:29 +0300

    Content completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Slider from "./components/Slider.vue";
+import Content from "./components/Content.vue";
 </script>
 <template>
   <Navbar />
   <Slider />
+  <Content />
 </template>
